b48760fd1d038e66a7c4d0ad9070e8c2cb4e93f4
Alae El Oula <alaaeloula@gmail.com>
2022-09-12 17:32:53 +0100

    first commit

---
+++ voielle.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+
+
+int main(){
+	 char c;
+	printf("enter un char\n");
+	scanf("%c",&c);
+	
+	switch( c )
+{
+	case 'a':
+			printf("voielle");
+			break;
+    case 'e':
+			printf("voielle");
+			break;
+	case 'i':
+	printf("voielle");
+	    break;
+	    case 'u':
+	printf("voielle");
+	    break;
+	    case 'o':
+	printf("voielle");
+	    break;
+	default:
+		   printf("n est pas un voielle");
+			break;
+}
+	
+	
+	
+}
